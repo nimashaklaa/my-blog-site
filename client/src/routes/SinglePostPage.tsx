b8a424542d8d533ee9eabc3c14ab00dc40267d7e
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2026-02-05 18:31:47 +0530

    Remove unused components and simplify the SinglePostPage layout.

--- client/src/routes/SinglePostPage.tsx
+++ client/src/routes/SinglePostPage.tsx
@@ -1,8 +1,7 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useUser, useAuth } from "@clerk/clerk-react";
 import Image from "../components/Image";
 import PostMenuActions from "../components/PostMenuActions";
-import Search from "../components/Search";
 import Comments from "../components/Comments";
 import PostContent from "../components/PostContent";
 import axios, { AxiosError } from "axios";
@@ -78,26 +77,23 @@ const SinglePostPage = () => {
 
   return (
     <div className="flex flex-col gap-8">
+      {/* cover image - full width like Write preview */}
+      {data.img && (
+        <div className="w-full rounded-xl overflow-hidden bg-gray-100 max-h-64">
+          <Image
+            src={data.img}
+            alt=""
+            className="w-full h-full object-cover max-h-64"
+          />
+        </div>
+      )}
       {/* detail */}
       <div className="flex gap-8">
         <div className="lg:w-3/5 flex flex-col gap-8">
           <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
             {data.title}
           </h1>
-          <div className="flex items-center gap-2 text-gray-400 text-sm">
-            <span>Written by</span>
-            <Link
-              to={`/posts?author=${data.user.username}`}
-              className="text-blue-800"
-            >
-              {data.user.username}
-            </Link>
-            <span>on</span>
-            <Link to={`/posts?cat=${data.category}`} className="text-blue-800">
-              {data.category}
-            </Link>
-            <span>{format(data.createdAt)}</span>
-          </div>
+          <div className="text-gray-400 text-sm">{format(data.createdAt)}</div>
           <p className="text-gray-500 font-medium">{data.desc}</p>
           {/* Clap Button */}
           <div className="flex items-center gap-4 mt-4">
@@ -131,11 +127,6 @@ const SinglePostPage = () => {
             </button>
           </div>
         </div>
-        {data.img && (
-          <div className="hidden lg:block w-2/5">
-            <Image src={data.img} w={600} className="rounded-2xl" />
-          </div>
-        )}
       </div>
       {/* content */}
       <div className="flex flex-col md:flex-row gap-12 justify-between">
@@ -145,58 +136,7 @@ const SinglePostPage = () => {
         </div>
         {/* menu */}
         <div className="px-4 h-max sticky top-8">
-          <h1 className="mb-4 text-sm font-medium">Author</h1>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-8">
-              {data.user.img && (
-                <Image
-                  src={data.user.img}
-                  className="w-12 h-12 rounded-full object-cover"
-                  w={48}
-                  h={48}
-                />
-              )}
-              <Link
-                to={`/posts?author=${data.user.username}`}
-                className="text-blue-800"
-              >
-                {data.user.username}
-              </Link>
-            </div>
-            <p className="text-sm text-gray-500">Author</p>
-            <div className="flex gap-2">
-              <Link to="/">
-                <Image src="facebook.svg" />
-              </Link>
-              <Link to="/">
-                <Image src="instagram.svg" />
-              </Link>
-            </div>
-          </div>
           <PostMenuActions post={data} />
-          <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
-          <div className="flex flex-col gap-2 text-sm">
-            <Link to="/posts" className="underline">
-              All
-            </Link>
-            <Link to="/posts?cat=web-design" className="underline">
-              Web Design
-            </Link>
-            <Link to="/posts?cat=development" className="underline">
-              Development
-            </Link>
-            <Link to="/posts?cat=databases" className="underline">
-              Databases
-            </Link>
-            <Link to="/posts?cat=seo" className="underline">
-              Search Engines
-            </Link>
-            <Link to="/posts?cat=marketing" className="underline">
-              Marketing
-            </Link>
-          </div>
-          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
-          <Search />
         </div>
       </div>
       <Comments postId={data._id} />
